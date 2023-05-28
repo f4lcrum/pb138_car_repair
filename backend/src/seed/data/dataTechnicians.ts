be375d3794f9030eb1275b4f0fe5a64250d0cad0
xbernat2 <xbernat2@fi.muni.cz>
2023-05-28 12:14:44 +0200

    feat: changed types for seeding data

--- backend/src/seed/data/dataTechnicians.ts
+++ backend/src/seed/data/dataTechnicians.ts
@@ -1,8 +1,8 @@
 import {
-  Role, User, Repair, RepairMaterial,
+  Role, User,
 } from '@prisma/client';
 
-const technicianJozef : (User & { repairs: (Repair & { material: RepairMaterial[] })[] }) = {
+const technicianJozef : (User ) = {
   id: '72e4eda6-5bd0-466c-8c56-b5405cd12e2f',
   email: 'jozino@azet.sk',
   firstName: 'Jozef',
@@ -14,24 +14,24 @@ const technicianJozef : (User & { repairs: (Repair & { material: RepairMaterial[
   password: '123456',
   isVerified: false,
   phoneNumber: '+421123456789',
-  repairs: [{
-    id: '2653e9cd-ac59-4d85-bd88-4a6a2c5ae315',
-    createdAt: new Date('2012-02-07T12:45:03.000Z'),
-    vehicleId: '2be2a276-60f5-4583-8dc1-3c2be9aae841',
-    workPrice: 30,
-    description: 'Vymena oleja',
-    mileage: 90000,
-    name: 'Oprava c.1',
-    technicianId: '72e4eda6-5bd0-466c-8c56-b5405cd12e2f',
-    resolvedAt: new Date('2012-02-25T12:45:03.000Z'),
-    material: [{
-      id: '41221bf6-d3d6-407b-bba7-ffe50f0275be',
-      name: '5V20 by Bosch',
-      description: '5V20 motorovy olej',
-      price: 20,
-      repairId: '2653e9cd-ac59-4d85-bd88-4a6a2c5ae315',
-    }],
-}],
+//   repairs: [{
+//     id: '2653e9cd-ac59-4d85-bd88-4a6a2c5ae315',
+//     createdAt: new Date('2012-02-07T12:45:03.000Z'),
+//     vehicleId: '2be2a276-60f5-4583-8dc1-3c2be9aae841',
+//     workPrice: 30,
+//     description: 'Vymena oleja',
+//     mileage: 90000,
+//     name: 'Oprava c.1',
+//     technicianId: '72e4eda6-5bd0-466c-8c56-b5405cd12e2f',
+//     resolvedAt: new Date('2012-02-25T12:45:03.000Z'),
+//     material: [{
+//       id: '41221bf6-d3d6-407b-bba7-ffe50f0275be',
+//       name: '5V20 by Bosch',
+//       description: '5V20 motorovy olej',
+//       price: 20,
+//       repairId: '2653e9cd-ac59-4d85-bd88-4a6a2c5ae315',
+//     }],
+// }],
 };
 
 export const allTechnicians = [
--- backend/src/seed/data/dataVehicles.ts
+++ backend/src/seed/data/dataVehicles.ts
@@ -1,9 +1,11 @@
 import type {
-  Vehicle, Repair, RepairMaterial,
+  Vehicle,
 } from '@prisma/client';
 
 // Vehicle & {brand: BrandModel}
-const seatIbiza: (Vehicle  & { repairs:(Repair & { material: RepairMaterial[] })[] }) = {
+const seatIbiza: (Vehicle
+    //& { repairs:(Repair & { material: RepairMaterial[] })[] }
+    ) = {
   id: '2be2a276-60f5-4583-8dc1-3c2be9aae841',
   ownerId: 'e62b71bd-c10b-41e6-aa26-6b7ba25c16b9',
   brandId: '875bc2f6-bd26-4f6a-ae29-1c3669021831',
@@ -13,12 +15,14 @@ const seatIbiza: (Vehicle  & { repairs:(Repair & { material: RepairMaterial[] })
   manufacturedAt: new Date('2010-03-05T16:00:00.000Z'),
   scrappedAt: null,
   deletedAt: null,
-  repairs: [
-  ],
+  // repairs: [
+  // ],
 
 };
 
-const porscheTaycan: (Vehicle  & { repairs:(Repair & { material: RepairMaterial[] })[] }) = {
+const porscheTaycan: (Vehicle
+    //& { repairs:(Repair & { material: RepairMaterial[] })[] }
+    ) = {
   id: '5f66633f-1b1f-42f9-ae44-21328e263bff',
   ownerId: 'b13ec116-0392-4db3-8684-9edb61f3e7a9',
   brandId: '755a4280-f745-11ed-b67e-0242ac120002',
@@ -28,12 +32,14 @@ const porscheTaycan: (Vehicle  & { repairs:(Repair & { material: RepairMaterial[
   manufacturedAt: new Date('2013-03-05T16:00:00.000Z'),
   scrappedAt: null,
   deletedAt: null,
-  repairs: [
-  ],
+  // repairs: [
+  // ],
 
 };
 
-const porscheCarrera: (Vehicle  & { repairs:(Repair & { material: RepairMaterial[] })[] }) = {
+const porscheCarrera: (Vehicle
+    //& { repairs:(Repair & { material: RepairMaterial[] })[] }
+    ) = {
   id: '6fbe873a-73e7-443a-b528-ead0d59ac747',
   ownerId: '31beee14-f744-11ed-b67e-0242ac120002',
   brandId: '6f439f5e-f745-11ed-b67e-0242ac120002',
@@ -43,12 +49,14 @@ const porscheCarrera: (Vehicle  & { repairs:(Repair & { material: RepairMaterial
   manufacturedAt: new Date('2016-03-05T16:00:00.000Z'),
   scrappedAt: null,
   deletedAt: null,
-  repairs: [
-  ],
+  // repairs: [
+  // ],
 
 };
 
-const bmwX6: (Vehicle  & { repairs:(Repair & { material: RepairMaterial[] })[] }) = {
+const bmwX6: (Vehicle
+    //& { repairs:(Repair & { material: RepairMaterial[] })[] }
+    ) = {
   id: 'fc986e3f-f341-417b-b180-a52aef37e049',
   ownerId: '38b610c6-f744-11ed-b67e-0242ac120002',
   brandId: '40fc1f92-dbc5-4595-ad7e-a61e677d2843',
@@ -58,15 +66,14 @@ const bmwX6: (Vehicle  & { repairs:(Repair & { material: RepairMaterial[] })[] }
   manufacturedAt: new Date('2018-03-05T16:00:00.000Z'),
   scrappedAt: null,
   deletedAt: null,
-  repairs: [
-  ],
+  // repairs: [
+  // ],
 
 };
 
-export const allVehicles: (Vehicle & {
-  repairs: (Repair & {
-      material: RepairMaterial[],
-  })[]})[] = [
+export const allVehicles: (Vehicle
+   //& {repairs: (Repair & {material: RepairMaterial[],})
+  )[] = [
   seatIbiza, 
   porscheTaycan,
   porscheCarrera,
