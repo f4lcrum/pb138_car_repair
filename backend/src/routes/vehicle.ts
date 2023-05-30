45c7781196a001f1982e683b8d860c762d37ab31
xbernat2 <xbernat2@fi.muni.cz>
2023-05-30 13:13:14 +0200

    feat: added create vehicle endpoint

--- backend/src/routes/vehicle.ts
+++ backend/src/routes/vehicle.ts
@@ -12,5 +12,5 @@ const vehicleRouteSpecific = `${vehicleRouteGeneric}/:id`;
 vehicleRouter.get(vehicleRouteGeneric, auth(Role.CLIENT, Role.ADMIN), VehicleController.readVehicles);
 vehicleRouter.delete(vehicleRouteSpecific, auth(Role.CLIENT, Role.ADMIN), VehicleController.deleteSpecificVehicle);
 vehicleRouter.get(vehicleRouteGenericSearch, auth(Role.CLIENT, Role.ADMIN), VehicleController.readSpecificVehicle);
-
+vehicleRouter.post(vehicleRouteGeneric, auth(Role.CLIENT), VehicleController.createVehicle);
 export default vehicleRouter;
