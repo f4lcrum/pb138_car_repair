59c596e1d1e63c2249a6031ddb81aa44c064a8c7
Kristian Oravec <xoravec2@fi.muni.cz>
2023-05-31 10:57:30 +0200

    fix: responses common data structure established

--- backend/src/repositories/common/responses.ts
+++ backend/src/repositories/common/responses.ts
@@ -1,29 +1,29 @@
 import type { Response } from 'express';
 
 export const unauthorizedRequestResponse = (res: Response, messageError: string) => {
-  res.status(401).send({ status: 'Unauthorized', error: messageError });
+  res.status(401).send({ error: messageError, data: null });
 };
 
 export const sendBadRequestResponse = (res: Response, messageError: String) => {
-  res.status(400).send({ status: 'failure', error: messageError });
+  res.status(400).send({ error: messageError, data: null });
 };
 
 export const notFoundRequestResponse = (res: Response) => {
-  res.status(404).send({ status: 'Not found', error: 'NotFound' });
+  res.status(404).send({ error: null, data: null });
 };
 
 export const receivedRequestResponse = (res: Response, inputData: any) => {
-  res.status(200).send(inputData);
+  res.status(200).send({ error: null, data: inputData });
 };
 
 export const createdSuccessRequestResponse = (res: Response, inputData: any) => {
-  res.status(201).send({ data: inputData });
+  res.status(201).send({ error: null, data: inputData });
 };
 
 export const backendErrorRequestResponse = (res: Response) => {
-  res.status(500).send({ status: 'Internal error' });
+  res.status(500).send({ error: null, data: null });
 };
 
 export const forbiddenRequestResponse = (res: Response, messageError: string) => {
-  res.status(403).send({ status: 'Forbidden', error: messageError });
+  res.status(403).send({ error: messageError, data: null });
 };
