d43765df7e86324e5957b8f582efd3ce7167d906
Adrian Veselka <adrian.veselka@devcors.com>
2023-06-07 20:45:36 +0200

    feat: added filter by brand

--- frontend/src/pages/vehicle/VehicleListPage.tsx
+++ frontend/src/pages/vehicle/VehicleListPage.tsx
@@ -2,8 +2,11 @@ import { FC, useState } from "react";
 import {
   Box,
   Button,
+  FormControl,
   Grid,
+  InputLabel,
   Paper,
+  SelectChangeEvent,
   Table,
   TableBody,
   TableCell,
@@ -18,14 +21,19 @@ import VehicleModal from "../../components/modals/VehicleModal";
 import { useSearchParams } from "react-router-dom";
 import { useVehicles } from "../../hooks/useVehicles";
 import { useQueryClient } from "react-query";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import { useBrands } from "../../hooks/useBrands.ts";
 
 const VehicleListPage: FC = () => {
   const [searchParams, _] = useSearchParams();
   const [vehicleModalOpen, setVehicleModalOpen] = useState(false);
   const [order, setOrder] = useState<"asc" | "desc">("asc");
   const queryClient = useQueryClient();
+  const [searchedBrand, setSearchedBrand] = useState<string>("NOT_SELECTED");
 
   const { data, isLoading } = useVehicles(searchParams);
+  const brands = useBrands();
 
   const handleSort = () => {
     const newOrder = order === "asc" ? "desc" : "asc";
@@ -33,6 +41,19 @@ const VehicleListPage: FC = () => {
     searchParams.set("sortOrder", newOrder);
     queryClient.invalidateQueries(["vehicles"]);
     setOrder(newOrder);
+  };
+
+  const handleBrandSearch = (event: SelectChangeEvent) => {
+    const newBrand = event.target.value;
+
+    if (newBrand === "NOT_SELECTED") {
+      searchParams.delete("brandName");
+    } else {
+      searchParams.set("brandName", newBrand);
+    }
+
+    queryClient.invalidateQueries(["vehicles"]);
+    setSearchedBrand(newBrand);
   };
 
   return (
@@ -49,6 +70,27 @@ const VehicleListPage: FC = () => {
             spacing={2}
             sx={{ marginTop: 2 }}
           >
+            {/*TODO: Style*/}
+            <Grid item sx={{ margin: 2 }}>
+              <FormControl fullWidth>
+                <InputLabel>Brand</InputLabel>
+                <Select
+                  value={searchedBrand}
+                  label="brand"
+                  onChange={handleBrandSearch}
+                >
+                  <MenuItem key={"NOT_SELECTED"} value={"NOT_SELECTED"}>
+                    Not selected
+                  </MenuItem>
+                  {brands.data &&
+                    Array.from(brands.data).map((brand) => (
+                      <MenuItem key={brand.id} value={brand.brand}>
+                        {brand.brand}
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+            </Grid>
             <Grid item>
               <Button
                 onClick={() => setVehicleModalOpen(true)}
@@ -56,9 +98,6 @@ const VehicleListPage: FC = () => {
               >
                 Add Vehicle
               </Button>
-            </Grid>
-            <Grid item>
-              <Button variant={"contained"}>Filter</Button>
             </Grid>
             <Grid item xs={12}>
               <TableContainer component={Paper}>
